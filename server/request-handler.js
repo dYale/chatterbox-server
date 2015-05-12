8e213c4cd21f741743a2bdac60c0bc8bdb4920a9
Dan Corman <dan.corman@gmail.com>
2015-05-12 17:02:31 -0500

    Completely refactored, tests aswell

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,83 +1,36 @@
-var qs = require('querystring');
-var fs = require('fs');
-var assert = require('assert');
-var _ = require('underscore');
-var localData = {};
-var messagesArray = [];
+var utils = require('./utils');
+
+var messages = [
+    {
+        text: "Hello Fred",
+        username: "Alice"
+    }
+];
 
 
-exports.requestHandler = function (request, response) {
-    var currentUrl = ['/classes/room1', '/send', '/classes/room', '/classes/messages', '/']
-    if (request.method === 'POST') {
-        if (currentUrl.indexOf(request.url) !== -1) {
-            var statusCode = 201;
-        } else {
-
-            var statusCode = 404;
-        }
-
-        var headers = defaultCorsHeaders;
-        headers["Content-Type"] = "application/json";
-        response.writeHead(statusCode, headers);
-
-        var body = '';
-        request.on('data', function (data) {
-            body += data;
-
-            if (body.length > 1e6) {
-                request.connection.destroy()
-            }
-        });
-
-        request.on('end', function () {
-            messagesArray.unshift(JSON.parse(body));
-        });
-
-        var r = {
-            result: ['SUCCESS']
-        }
-        response.end(JSON.stringify(r))
-
-    } else if(request.method === 'GET' || request.method ==='OPTIONS'){
-
-      console.log(request.url)
-        if (currentUrl.indexOf(request.url) !== -1 && request.url !== './arglebargle') {
-            var statusCode = 200;
-        } else {
-
-            var statusCode = 404;
-        }
-        var headers = defaultCorsHeaders;
-        headers['Content-Type'] = "application/json"
-
-        console.log("Serving request type " + request.method + " for url " + request.url);
-
-        response.writeHead(statusCode, headers);
-
-
-
-
-
-        // Make sure to always call response.end() - Node may not send
-        // anything back to the client until you do. The string you pass to
-        // response.end() will be the body of the response - i.e. what shows
-        // up in the browser.
-
-        //
-        // Calling .end "flushes" the response's internal buffer, forcing
-        // node to actually send all the data over to the client.
-
-
-        response.end(JSON.stringify({
-            results: messagesArray
-        }))
-    } 
+var actions = {
+    'GET': function(request, response){
+      utils.sendResponse(response, {results: messages}, 200)
+    },
+    'POST': function(request, response){
+      utils.collectData(request, function(message){
+        messages.unshift(message)
+        utils.sendResponse(response, message, 201)
+      });
+    },
+    'OPTIONS': function(request, response){
+      utils.sendResponse(response, null)
+    }
 };
 
+module.exports = function (request, response) {
+var action = actions[request.method]
+if(action){
 
-defaultCorsHeaders = {
-    "access-control-allow-origin": "*",
-        "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "access-control-allow-headers": "content-type, accept",
-        "access-control-max-age": 10 // Seconds.
+    action(request, response);
+} else { 
+    utils.sendResponse(response, "Not Found", 404)
+}
+
+
 };
